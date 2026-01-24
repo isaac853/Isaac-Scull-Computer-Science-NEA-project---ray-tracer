e9a48414fbadf15fa963d37da8b615c374f50606
Isaac Scull <isaac.scull@gmail.com>
2026-01-24 16:39:32 +0000

    tidy up

--- src/PIxelCanvas.hpp
+++ src/PIxelCanvas.hpp
@@ -1,14 +1,8 @@
 #pragma once
 
-// #include <iostream>
 #include <vector>
-// #include <cstring>
 #include <cstdint>
-// #include <SDL.h>
 #include <GL/gl.h>
-// #include <imgui.h>
-// #include <imgui_impl_sdl2.h>
-// #include <imgui_impl_opengl3.h>
 
 
 // Simple RGB pixel buffer class
