7356ad519278e5999472ab3014ffb72cb268adeb
Isaac Scull <isaac.scull@gmail.com>
2026-02-01 20:22:32 +0000

    Correct x-axis sign

--- src/ray_tracer/Camera.cpp
+++ src/ray_tracer/Camera.cpp
@@ -62,7 +62,7 @@ void Camera::render(
     pixelCanvas.forEach(
         [&](const int x, const int y, const int w, const int h, uint8_t &r, uint8_t &g, uint8_t &b) {
 
-            const floating cx = (offset_x + (floating)x);
+            const floating cx = -(offset_x + (floating)x);
             const floating cy = (offset_y + (floating)y);
             const floating cz = -focalLength; 
 
