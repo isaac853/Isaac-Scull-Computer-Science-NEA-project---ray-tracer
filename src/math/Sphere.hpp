bf955735d9c6a42c374cbc42f77ce073e09e4da4
Isaac Scull <isaac.scull@gmail.com>
2026-02-08 17:37:31 +0000

    Check sphere is in front of viewer

--- src/math/Sphere.hpp
+++ src/math/Sphere.hpp
@@ -9,20 +9,26 @@ class Sphere {
    public:
    //sphere radius r about (0,0,0)
     Sphere(floating r) : radius(r), radiusSquared(r*r) {}
-    
-    inline floating descriminantcalc(Vector3 &start, Vector3 &dir){
-        const floating knum = start.lengthSquared();
-        const floating kden = start.dot(dir);
-
-        //desc = b^2 - 4ac
-
-        floating descByFour = kden*kden - knum + radiusSquared;
-        return descByFour;
-    }
 
     inline bool intersects(Vector3 &start, Vector3 &dir, Vector3 &result){      
-        const floating descByFour = descriminantcalc(start, dir);
-        return descByFour >= 0;
+        const floating sdots = start.lengthSquared();
+        const floating sdotd = start.dot(dir);
+
+        //desc = b^2 - 4ac
+        const floating descByFour = sdotd*sdotd - sdots + radiusSquared;
+        if (descByFour < 0) return false;
+
+        const floating rootDescByFour = std::sqrt(descByFour); //TODO check how fast this is
+
+        const floating k1 = -sdotd + rootDescByFour;
+        const floating k2 = -sdotd - rootDescByFour;
+
+        if (k1 < 0.0 && k2 < 0.0) return false;
+
+        //pick the smallest positive value of k
+        const floating kpm = (k1 >= 0.0 && k1 < k2) || k2 < 0.0 ? k1 : k2;
+
+        return true;
     }
 
     inline Sphere& operator=(const Sphere& other){
